7af2017b99b7e196240f0c5778d08abe848cbbca
Shashwat Rauthan <64864041+shashwatrauthan@users.noreply.github.com>
2022-04-12 18:52:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const mongoURI = process.env.MONGO_URI;
+// const mongoURI = process.env.MONGO_URI;
+const mongoURI = "mongodb+srv://shashwatrauthan:Qwerty123456@cloud-notes.simea.mongodb.net/cloudNotes?retryWrites=true&w=majority";
+
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
-        console.log("this location"+mongoURI);
         console.log("Connected to Mongo.");
     })
 }
